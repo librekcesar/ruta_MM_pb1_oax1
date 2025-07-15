ed88587c522a101a3323e126abebbde1966a4571
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 15:24:47 -0600

    Add files via upload

---
+++ estaciones_pb1_oax1_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_pb1_oax1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "19.108275213759235, -98.20874612959832", "description": "PB1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -98.2086926, 19.1082811, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Cto. Sur 1 20-32, El Rosario, El Rosario, Oax., México", "description": "OAX1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -96.6900198, 17.042826, 0.0 ] } }
+]
+}
---
+++ paradas_pb1_oax1_copia.js
@@ -0,0 +1,11 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_pb1_oax1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Autopista Cuacnopalan-Tehuacan-Oaxaca Km 38.5, 75825 Tehuacán, Puebla", "layer": "PEMEX_PB_OAX", "direccion": "Autopista Cuacnopalan-Tehuacan-Oaxaca Km 38.5, 75825 Tehuacán, Puebla" }, "geometry": { "type": "Point", "coordinates": [ -97.4537696, 18.4970473, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex", "description": "Autopista Mexico- Orizaba Km.202,700, 75515 Pue.", "layer": "PEMEX_PB_OAX", "direccion": "Autopista Mexico- Orizaba Km.202,700, 75515 Pue." }, "geometry": { "type": "Point", "coordinates": [ -97.505425, 18.830238, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estación Nitrogas COMA", "description": "Autop. Orizaba - Puebla S/N-Km 158.4 Cuerpo A, 75200 Tepeaca, Pue.", "layer": "PEMEX_PB_OAX", "direccion": "Autop. Orizaba - Puebla S/N-Km 158.4 Cuerpo A, 75200 Tepeaca, Pue." }, "geometry": { "type": "Point", "coordinates": [ -97.8806478, 18.9992948, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Capufe Comunicaciones", "description": "68220 Oax., México", "layer": "PEMEX_PB_OAX", "direccion": "68220 Oax., México" }, "geometry": { "type": "Point", "coordinates": [ -96.9176805, 17.2770914, 0.0 ] } }
+]
+}
